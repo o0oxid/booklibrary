bec38fb80cc0bbee0079074ff836e9eca5ad01ee
Oleg Khoruzhenko <Oleg.Khoruzhenko@bazaarvoice.com>
2017-04-12 08:24:36 -0500

    Getting rid of CatalogStorageInMemoryAbstract

--- src/main/java/com/mycompany/app/catalog/CatalogBookStorageInMemory.java
+++ src/main/java/com/mycompany/app/catalog/CatalogBookStorageInMemory.java
@@ -1,6 +1,7 @@
 package com.mycompany.app.catalog;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -8,7 +9,31 @@ import java.util.stream.Collectors;
 /**
  * Created by okhoruzhenko on 3/27/17.
  */
-public class CatalogBookStorageInMemory extends CatalogStorageInMemoryAbstract<CatalogEntryBook>  {
+public class CatalogBookStorageInMemory implements CatalogStorage<CatalogEntryBook>  {
+    protected Collection<CatalogEntryBook> catalog;
+
+    public CatalogBookStorageInMemory(Collection<CatalogEntryBook> catalog) {
+        this.catalog = catalog;
+    }
+
+    public CatalogBookStorageInMemory() {
+        catalog = new ArrayList<>();
+    }
+
+    public void add(CatalogEntryBook entry) {
+        catalog.add(entry);
+    }
+    public void remove(CatalogEntryBook entry) {
+        catalog.remove(entry);
+    }
+
+    @Override
+    public Set<CatalogEntryBook> lookup(final String text) {
+        Set<CatalogEntryBook> result = catalog.stream()
+                .filter(e -> e.getTitle().matches(text))
+                .collect(Collectors.toSet());
+        return result;
+    }
     public Set<CatalogEntryBook> lookup(final CatalogEntryBook entry) {
         Set<CatalogEntryBook> result = catalog.stream()
                 .filter(e -> e.matches(entry))
--- src/main/java/com/mycompany/app/catalog/CatalogMagazineStorageInMemory.java
+++ src/main/java/com/mycompany/app/catalog/CatalogMagazineStorageInMemory.java
@@ -1,12 +1,39 @@
 package com.mycompany.app.catalog;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
  * Created by okhoruzhenko on 4/10/17.
  */
-public class CatalogMagazineStorageInMemory extends CatalogStorageInMemoryAbstract<CatalogEntryMagazine>  {
+public class CatalogMagazineStorageInMemory implements CatalogStorage<CatalogEntryMagazine>  {
+    protected Collection<CatalogEntryMagazine> catalog;
+
+    public CatalogMagazineStorageInMemory(Collection<CatalogEntryMagazine> catalog) {
+        this.catalog = catalog;
+    }
+
+    public CatalogMagazineStorageInMemory() {
+        catalog = new ArrayList<>();
+    }
+
+    public void add(CatalogEntryMagazine entry) {
+        catalog.add(entry);
+    }
+    public void remove(CatalogEntryMagazine entry) {
+        catalog.remove(entry);
+    }
+
+    @Override
+    public Set<CatalogEntryMagazine> lookup(final String text) {
+        Set<CatalogEntryMagazine> result = catalog.stream()
+                .filter(e -> e.getTitle().matches(text))
+                .collect(Collectors.toSet());
+        return result;
+    }
+    @Override
     public Set<CatalogEntryMagazine> lookup(final CatalogEntryMagazine entry) {
         Set<CatalogEntryMagazine> result = catalog.stream()
                 .filter(e -> e.matches(entry))
